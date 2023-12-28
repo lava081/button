e2dcc2202b81ee5594503dcb54e92b427abcad07
岩浆 <linboyan5438081@163.com>
2023-12-29 03:50:53 +0800

    flower-plugin

---
+++ flower-plugin.js
@@ -0,0 +1,118 @@
+export default class Button {
+  constructor() {
+    this.plugin = {
+      name: "flower-plugin",
+      dsc: "flower-plugin",
+      priority: 100,
+      rule: [
+        {
+            reg: '^#*(10|[武器池常驻]*([一二三四五六七八九]?[十百]+)|抽)[连抽卡奖][123武器池常驻]*$',
+            fnc: 'gacha'
+          },
+          {
+            reg: '(^#*定轨|^#定轨(.*))$',
+            fnc: 'gacha'
+          },
+          {
+            reg: '^#?(我的|领取|查询|查看)(纠缠|相遇|粉|蓝)(之缘|球)?$',
+            fnc: 'gacha'
+          },
+          {
+            reg: '^#*单抽[12武器池常驻]*$',
+            fnc: 'gacha'
+          },
+          {
+            reg: '^#*(原神)?转生$',
+            fnc: 'relife'
+          }
+      ]
+    }
+  }
+
+  gacha(e){
+    const button = []
+    const game = e.isSr?'星铁':''
+    const weapon = e.isSr?'光锥':'武器'
+    let number = e.msg.match(/(([一二三四五六七八九]?[单十百10])|抽)[连抽卡奖]/)
+    let type = e.msg.match(/武器|常驻|2/)
+
+    if(type == null){
+      type = []
+      type[0] = ''
+    }
+    type[0] = type[0].replace(/武器/, '武器池').replace(/常驻/, '常驻池')
+
+    let list = [
+      { label: `up池1`, data: `/${game}${number[0]}` },
+      { label: `up池2`, data: `/${game}${number[0]}2` },
+      { label: `常驻池`, data: `/${game}${number[0]}常驻池` },
+    ]
+    button.push(...toButton(list))
+
+    list = [
+      { label: `定轨`, data: `/${game}定轨` },
+      { label: `${weapon}池`, data: `/${game}${number[0]}${weapon}池` },
+    ]
+    button.push(...toButton(list))
+
+    list = [
+      { label: `单抽`, data: `/${game}单抽${type[0]}` },
+      { label: `十连`, data: `/${game}十连${type[0]}` },
+      { label: `九十连`, data: `/${game}九十连${type[0]}`},
+    ]
+    button.push(...toButton(list))
+
+    number = ['派蒙','十连','单抽']
+    number[0] = number[Math.floor(Math.random()*3)]
+    type = ['','2','常驻池',`${weapon}池`]
+    type[0] = type[Math.floor(Math.random()*4)]
+    if(number[0] == '派蒙')
+      list = [
+        { label: `交给派蒙抽`, data: `/抽卡的钱被小派蒙拿去买甜甜花酿鸡了` },
+      ]
+    else
+      list = [
+        { label: `交给派蒙抽`, data: `/${game}${number[0]}${type[0]}` },
+      ]
+    button.push(...toButton(list))
+
+    return button
+  }
+
+  relife(){
+    const button = [
+        { label: `重生提瓦特之我是`, data: `/转生` },
+    ]
+    return toButton(button)
+  }
+}
+
+function toButton(list, line = 3) {
+  let button = []
+  let arr = []
+  let index = 1
+  for (const i of list) {
+    arr.push({
+      id: String(Date.now()),
+      render_data: {
+        label: i.label,
+        style: 1
+      },
+      action: {
+        type: 2,
+        permission: { type: 2 },
+        data: i.data,
+        enter: true,
+        unsupport_tips: "code: 45",
+      }
+    })
+    if (index % line == 0 || index == list.length) {
+      button.push({type: 'button',
+        buttons: arr
+      })
+      arr = []
+    }
+    index++
+  }
+  return button
+}
