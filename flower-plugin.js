c124468aaf7b22d6f943da28c2391fe0c6a4c23a
lava081 <linboyan5438081@163.com>
2024-01-22 18:31:07 +0800

    适应callback

--- flower-plugin.js
+++ flower-plugin.js
@@ -49,49 +49,49 @@ export default class Button {
     type[0] = type[0].replace(/武器/, '武器池').replace(/光锥/, '光锥池').replace(/常驻/, '常驻池')
 
     let list = [
-      { label: `up池1`, data: `/${game}${number[0]}`, enter: true },
-      { label: `up池2`, data: `/${game}${number[0]}2`, enter: true },
-      { label: `常驻池`, data: `/${game}${number[0]}常驻池`, enter: true },
+      { label: `up池1`, callback: `/${game}${number[0]}`},
+      { label: `up池2`, callback: `/${game}${number[0]}2`},
+      { label: `常驻池`, callback: `/${game}${number[0]}常驻池`},
     ]
-    button.push(...Bot.Button(list))
+    button.push(list)
 
     list = [
-      { label: `定轨`, data: `/${game}定轨`, enter: true },
-      { label: `${weapon}池`, data: `/${game}${number[0]}${weapon}池`, enter: true },
+      { label: `定轨`, callback: `/${game}定轨`},
+      { label: `${weapon}池`, callback: `/${game}${number[0]}${weapon}池`},
     ]
-    button.push(...Bot.Button(list))
+    button.push(list)
 
     list = [
-      { label: `单抽`, data: `/${game}单抽${type[0]}`, enter: true },
-      { label: `十连`, data: `/${game}十连${type[0]}`, enter: true },
+      { label: `单抽`, callback: `/${game}单抽${type[0]}`},
+      { label: `十连`, callback: `/${game}十连${type[0]}`},
     ]
     if ([4, '4'].includes(e.bot.config.markdown.type))
-      list.push({ label: `保底`, data: `/${game}${(type[0].match(/武器|光锥/))?'八十':'九十'}连${type[0]}`, enter: true })
-    button.push(...Bot.Button(list))
+      list.push({ label: `保底`, callback: `/${game}${(type[0].match(/武器|光锥/))?'八十':'九十'}连${type[0]}`})
+    button.push(list)
 
     type = ['','2','常驻池',`${weapon}池`]
-    type[0] = type[Math.floor(Math.random()*4)]
+    type[0] = type[Math.floor(Math.random()*type.length)]
     number = ['十连','十连','单抽']
-    if ([1, '1', 4, '4'].includes(e.bot.config.markdown.type))
-    number.push(`${(type[0].match(/武器|光锥/))?'八十':'九十'}连`)
-    number[0] = number[Math.floor(Math.random()*4)]
+    if ([4, '4'].includes(e.bot.config.markdown.type))
+      number.push(`${(type[0].match(/武器|光锥/))?'八十':'九十'}连`)
+    number[0] = number[Math.floor(Math.random()*number.length)]
 
     if(number[0] == '派蒙')
       list = [
-        { label: `交给派蒙抽`, data: `抽卡的钱被小派蒙拿去买甜甜花酿鸡了`, enter: true },
+        { label: `交给派蒙抽`, callback: `抽卡的钱被小派蒙拿去买甜甜花酿鸡了`},
       ]
     else
       list = [
-        { label: `交给派蒙抽`, data: `/${game}${number[0]}${type[0]}`, enter: true },
+        { label: `交给派蒙抽`, callback: `/${game}${number[0]}${type[0]}`},
       ]
-    button.push(...Bot.Button(list))
+    button.push(list)
 
-    return button
+    return Bot.Button(button)
   }
 
   relife(){
     const button = [
-        { label: `重生提瓦特之我是`, data: `/转生`, enter: true },
+        { label: `重生提瓦特之我是`, callback: `/转生`},
     ]
     return Bot.Button(button)
   }
