a393e5e5c75e461ccd7dc315a119bedc0fddafe4
岩浆 <linboyan5438081@163.com>
2024-03-15 01:49:14 +0800

    优化性能

--- miao-plugin.js
+++ miao-plugin.js
@@ -1,58 +1,6 @@
 /* 原作者：[风间叶](https://github.com/xiaoye12123/), [Lain.](https://github.com/Zyy955/), [夜](https://github.com/yeyeyye-eee) */
 import Character from '../../../../miao-plugin/models/Character.js'
-import { alias as alias_gs } from '../../../../miao-plugin/resources/meta-gs/character/alias.js'
-import { alias as alias_sr } from '../../../../miao-plugin/resources/meta-sr/character/alias.js'
-import { extraChars as extra_gs } from '../../../../miao-plugin/resources/meta-gs/character/extra.js'
-
-const alias = {
-  gs: {},
-  sr: {}
-}
-refreshAlias()
-
-/** 刷新别名列表 */
-async function refreshAlias () {
-  let custom_gs = {}
-  try {
-    // 尝试动态导入 customCharacters
-    const { customCharacters } = await import('../../../../miao-plugin/config/character.js')
-    custom_gs = customCharacters
-  } catch (error) {
-    const { customCharacters } = await import('../../../../miao-plugin/config/character_default.js')
-    custom_gs = customCharacters
-  }
-  for (const origin_name in alias_gs) {
-    if (alias_gs[origin_name]) {
-      alias.gs[origin_name] = alias_gs[origin_name].split(',')
-    } else {
-      alias.gs[origin_name] = []
-    }
-    alias.gs[origin_name].push(origin_name)
-  }
-  for (const origin_name in extra_gs) {
-    if (extra_gs[origin_name]) {
-      alias.gs[origin_name] = extra_gs[origin_name].split(',')
-    } else {
-      alias.gs[origin_name] = []
-    }
-  }
-  for (const origin_name in custom_gs) {
-    if (!alias.gs[custom_gs[origin_name][0]]) {
-      alias.gs[custom_gs[origin_name][0]] = custom_gs[origin_name]
-    } else {
-      alias.gs[custom_gs[origin_name][0]].push(...custom_gs[origin_name])
-    }
-  }
-
-  for (const origin_name in alias_sr) {
-    if (alias_sr[origin_name]) {
-      alias.sr[origin_name] = alias_sr[origin_name].split(',')
-    } else {
-      alias.sr[origin_name] = []
-    }
-    alias.sr[origin_name].push(origin_name)
-  }
-}
+import Meta from '../../../../miao-plugin/components/Meta.js'
 
 /**
  * 在给定的文本中搜索与别名对象中任何别名匹配的字符串
@@ -63,11 +11,9 @@ async function refreshAlias () {
 
 async function findCharacter (text, isSr) {
   const game = isSr ? 'sr' : 'gs'
-  for (const origin_name in alias[game]) {
-    for (const nickname of alias[game][origin_name]) {
-      if (text.includes(nickname)) {
-        return origin_name
-      }
+  for (const nickname of Meta.getAlias(game, 'char')) {
+    if (text.includes(nickname)) {
+      return nickname
     }
   }
   return null
