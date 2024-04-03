fdc1ab251bf2631989863129372170ec95bb98eb
岩浆 <linboyan5438081@163.com>
2024-04-03 10:39:30 +0800

    fix: [xiaoyao-cvs-plugin]匹配更新小助手记录
    但是按钮里先不添加

--- xiaoyao-cvs-plugin.js
+++ xiaoyao-cvs-plugin.js
@@ -18,7 +18,7 @@ export default class Button {
           fnc: 'note'
         },
         {
-          reg: '^#*(原神|星铁)?(((更新|获取|导出)?抽卡记录)|((全部)?(抽卡|集录|抽奖|角色|武器|常驻|up|新手|光锥|全部)池*(记录|祈愿|分析|统计))|((记录|抽卡|安卓|苹果|电脑|pc|ios)帮助))$',
+          reg: '^#*(原神|星铁)?(((更新|获取|导出)?(抽卡|小助手)记录)|((全部)?(抽卡|集录|抽奖|角色|武器|常驻|up|新手|光锥|全部)池*(记录|祈愿|分析|统计))|((记录|抽卡|安卓|苹果|电脑|pc|ios)帮助))$',
           fnc: 'gacha'
         },
         {
@@ -89,19 +89,17 @@ export default class Button {
     const button = [
       { label: '角色记录', data: `#${game}角色池记录` },
       { label: '武器记录', data: `#${game}${game ? '光锥' : '武器'}池记录` },
-      
       { label: '集录记录', data: `#${game}集录池记录` },
+
       { label: '常驻记录', data: `#${game}常驻池记录` },
-      
       { label: '全部记录', data: `#${game}全部记录` },
       { label: '米游社登录', data: '#扫码登录' },
       
       { label: '角色统计', data: `#${game}角色池统计` },
       { label: '武器统计', data: `#${game}${game ? '光锥' : '武器'}池统计` },
-      
       { label: '集录统计', data: `#${game}集录池统计` },
+
       { label: '常驻统计', data: `#${game}常驻池统计` },
-      
       { label: '全部统计', data: `#${game}全部统计` },
 
     ]
